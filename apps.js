b38142a8049b2589c62374d86794d54869419511
Saad Afzal <126021044+saadabcafzal@users.noreply.github.com>
2023-06-11 17:08:54 +0500

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,16 @@
+let typingText=document.querySelector(".change_txt");
+let Designation=["Front End Developer","Back End Developer","Network Marketer"];
+let arrayIndex=1;
+function ChangingTxt(){
+    if(arrayIndex <Designation.length) {
+        typingText.innerHTML=Designation[arrayIndex];
+        arrayIndex=arrayIndex +1;
+    }
+    else {
+        arrayIndex=0;
+        typingText.innerHTML=Designation[arrayIndex];
+        arrayIndex=arrayIndex +1;
+    }
+}
+ChangingTxt();
+setInterval(ChangingTxt,5000);
